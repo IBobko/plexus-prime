704fa9478c8bdb050bab6651a802be948bbda687
Igor Bobko <iibobko@gmail.com>
2023-07-08 18:17:00 -0700

    Refactor server.py and test_digits_model.py
    - Refactor server.py:
      - Organize imports and reorder them
      - Remove redundancy by moving digit reshaping and prediction to separate function 'process_digit'
      - Clean up predict function by removing print statements and unnecessary code
      - Add 'picture' endpoint to fetch a specific image, convert it to grayscale, resize to 28x28 and return as JSON
    - Refactor test_digits_model.py:
      - Remove unused mnist dataset import
      - Add function 'generate_seven_array' that generates 2D numpy array representing '7'
    
    The changes improve the code readability and maintainability, and add new functionality for fetching images in server.py

--- server.py
+++ server.py
@@ -1,65 +1,46 @@
-import numpy as np
-from tensorflow.keras.models import load_model
+import traceback
 
+import numpy as np
+import tensorflow as tf
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import traceback
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 CORS(app)
 
-# Загружаем сохраненную модель
+# Load the saved model
 model = load_model('digits.h5')
 
-# Загружаем или создаем ваш 28x28 массив
-# Здесь мы создаем заглушку для примера
-digit = np.zeros((28,28))
 
-# Не забудьте нормализовать ваши данные, если это было сделано во время обучения
-digit = digit / 255.0
-
-# Измените форму массива, чтобы он соответствовал форме входных данных модели
-digit = digit.reshape(1, 28, 28, 1)
-
-# Делаем предсказание
-prediction = model.predict(digit)
-
-# Получаем наиболее вероятный класс
-predicted_class = np.argmax(prediction)
-
-print("Predicted class is: ", predicted_class)
-
-
-
-
+def process_digit(digit):
+    digit = digit.reshape(1, 28, 28, 1)
+    return model.predict(digit)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json(force=True)
-        print(data)
         digit = np.array(data)
-        digit = 1-digit
-        print(digit)
-        digit = digit.reshape(1, 28, 28, 1)
-        # Не забудьте нормализовать ваши данные, если это было сделано во время обучения
-        #digit = digit / 255.0
-
-        # Измените форму массива, чтобы он соответствовал форме входных данных модели
-
-        # Делаем предсказание
-        prediction = model.predict(digit)
-
-        # Получаем наиболее вероятный класс
+        prediction = process_digit(digit)
         predicted_class = np.argmax(prediction)
-
-        print("Predicted class is: ", predicted_class)
-
-
         return jsonify({'prediction': int(predicted_class)})
     except Exception as e:
         return jsonify({'error': 'An error occurred during prediction.', 'trace': traceback.format_exc()})
+
+
+@app.route('/picture', methods=['GET'])
+def picture():
+    custom_image = tf.keras.preprocessing.image.load_img('./mnist_img/mnist_image_0_label_7.png',
+                                                         color_mode='grayscale',
+                                                         target_size=(28, 28))
+    # Convert the image to numpy array
+    custom_image_array = tf.keras.preprocessing.image.img_to_array(custom_image)
+    # Convert to 28x28 array
+    custom_image_array_28x28 = np.squeeze(custom_image_array)
+    return jsonify({'data': custom_image_array_28x28.tolist()})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- test_digits_model.py
+++ test_digits_model.py
@@ -1,23 +1,92 @@
+import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
 
-# Загружаем MNIST датасет
-(_, _), (x_test, y_test) = mnist.load_data()
 
-# Нормализуем значения пикселей
-x_test = x_test / 255.0
+def generate_seven_array():
+    return np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
-# Преобразуем целевую переменную в категориальный формат
-y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
 
-# Добавляем еще одно измерение к нашим данным
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+# Load the custom image in PNG format
+custom_image = tf.keras.preprocessing.image.load_img('./mnist_img/mnist_image_0_label_7.png', color_mode='grayscale',
+    target_size=(28, 28))
 
-# Загружаем модель
+# Convert the image into a numpy array
+custom_image_array = tf.keras.preprocessing.image.img_to_array(custom_image)
+
+# Load the model
 model = tf.keras.models.load_model('digits.h5')
 
-# Оцениваем модель на тестовых данных MNIST
-score = model.evaluate(x_test, y_test, verbose=0)
+# Normalize the custom image
+custom_image_array = custom_image_array / 255.0
 
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+print(custom_image_array)
+
+# Add another dimension to the image
+custom_image_array = custom_image_array.reshape(1, 28, 28, 1)
+
+# Set the label for the custom image
+custom_image_label = 7
+custom_image_label = tf.one_hot(custom_image_label, depth=10)
+
+# Evaluate the model on the custom image
+custom_image_score = model.evaluate(custom_image_array, np.array([custom_image_label]), verbose=0)
+
+print('Test loss on the custom image:', custom_image_score[0])
+print('Test accuracy on the custom image:', custom_image_score[1])
+
+# Get the model's prediction for the custom image
+custom_image_prediction = model.predict(custom_image_array)
+
+# Generate the "seven" array and reshape it
+seven_array = generate_seven_array().reshape(1, 28, 28, 1)
+
+# Get the model's prediction for the "seven" array
+seven_array_prediction = model.predict(seven_array)
+
+# Extract the index of the class with the highest probability for the custom image
+custom_image_predicted_class = tf.argmax(custom_image_prediction[0]).numpy()
+# Print the predicted digit for the custom image
+print('Predicted digit from the custom image:', custom_image_predicted_class)
+
+# Extract the index of the class with the highest probability for the "seven" array
+seven_array_predicted_class = tf.argmax(seven_array_prediction[0]).numpy()
+# Print the predicted digit for the "seven" array
+print('Predicted digit from the "seven" array:', seven_array_predicted_class)
+
+# Set the label for the "seven" array
+seven_array_label = 7
+seven_array_label = tf.one_hot(seven_array_label, depth=10)
+
+# Evaluate the model on the "seven" array
+seven_array_score = model.evaluate(seven_array, np.array([seven_array_label]), verbose=0)
+
+print('Test loss on the "seven" array:', seven_array_score[0])
+print('Test accuracy on the "seven" array:', seven_array_score[1])
+
